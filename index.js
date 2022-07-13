3a2c98e3f3af11da75d4c9acf5b4a869395fd8db
CosmicSpectrum <66248422+CosmicSpectrum@users.noreply.github.com>
2022-07-13 16:47:50 +0300

    port change

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname+'/broadcaster-ui/build/index.html'));
 })
 
-server.listen(3001, ()=>{
+server.listen(80, ()=>{
     console.log('Server listening on 3001');
 })
 
